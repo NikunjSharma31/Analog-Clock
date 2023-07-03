cc89e29586fba21277483089ca4fb0559fce6f09
Nikunj Sharma <sharmanicktop@gmail.com>
2023-07-03 17:34:41 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,25 @@
+let hour = document.getElementById('hr');
+let minute = document.getElementById('min');
+let second = document.getElementById('sec');
+
+const Time = () =>{
+    let date= new Date();
+
+    let hh = date.getHours();
+    let mm = date.getMinutes();
+    let ss = date.getSeconds();
+
+    let hourRotate = 30*hh + mm/2;
+    let minuteRotate = 6*mm;
+    let secondRotate = 6*ss;
+
+    hour.style.transform = `rotate(${hourRotate}deg)`;
+    let p = minute.style.transform = `rotate(${minuteRotate}deg)`;
+    second.style.transform = `rotate(${secondRotate}deg)`;
+    // console.log(p);
+    
+}
+Time();
+setInterval(()=>{
+    Time()
+}, 1000)
